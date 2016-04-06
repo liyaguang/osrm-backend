9b5de6f00a3cc09d36d77f52c4241077d38bcfe1
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-04-06 23:13:21 +0200

    Use different formulat for latToY because its faster

--- src/util/coordinate_calculation.cpp
+++ src/util/coordinate_calculation.cpp
@@ -309,9 +309,8 @@ FloatLatitude yToLat(const double y)
 
 double latToY(const FloatLatitude latitude)
 {
-    const double normalized_lat = 90. + static_cast<double>(latitude);
-
-    const double y = RAD_TO_DEGREE * std::log(std::tan(normalized_lat * DEGREE_TO_RAD * 0.5));
+    const double f = std::sin(DEGREE_TO_RAD * static_cast<double>(latitude));
+    const double y = RAD_TO_DEGREE * 0.5 * std::log((1 + f) / (1 - f));
     const auto clamped_y = std::max(-180., std::min(180., y));
     return clamped_y;
 }
