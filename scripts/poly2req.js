958f53eea6765b169195302202cfb7d48da39abd
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-04-07 11:20:24 +0200

    Make poly file optional and default to planet

--- scripts/poly2req.js
+++ scripts/poly2req.js
@@ -10,26 +10,39 @@ let url_template = "http://127.0.0.1:5000/route/v1/driving/{coords}?steps=false&
 let coord_template = "{lon},{lat}";
 let coords_separator = ";";
 
-let monaco_poly_path = process.argv[2];
-let poly_data = fs.readFileSync(monaco_poly_path, 'utf-8');
-
-// lets assume there is only one ring
-// cut of name and ring number and the two END statements
-let coordinates = poly_data.split('\n')
-  .filter((l) => l != '')
-  .slice(2, -2).map((coord_line) => coord_line.split(' ')
-  .filter((elem) => elem != ''))
-  .map((coord) => [parseFloat(coord[0]), parseFloat(coord[1])]);
-
 var sw = [Number.MAX_VALUE, Number.MAX_VALUE];
 var ne = [Number.MIN_VALUE, Number.MIN_VALUE];
 
-coordinates.forEach((c) => {
-  sw[0] = Math.min(sw[0], c[0]);
-  sw[1] = Math.min(sw[1], c[1]);
-  ne[0] = Math.max(ne[0], c[0]);
-  ne[1] = Math.max(ne[1], c[1]);
-});
+if (process.argv.length > 2 && process.argv[2] != "planet")
+{
+  let monaco_poly_path = process.argv[2];
+  let poly_data = fs.readFileSync(monaco_poly_path, 'utf-8');
+
+  // lets assume there is only one ring
+  // cut of name and ring number and the two END statements
+  let coordinates = poly_data.split('\n')
+    .filter((l) => l != '')
+    .slice(2, -2).map((coord_line) => coord_line.split(' ')
+    .filter((elem) => elem != ''))
+    .map((coord) => [parseFloat(coord[0]), parseFloat(coord[1])]);
+
+  coordinates.forEach((c) => {
+    sw[0] = Math.min(sw[0], c[0]);
+    sw[1] = Math.min(sw[1], c[1]);
+    ne[0] = Math.max(ne[0], c[0]);
+    ne[1] = Math.max(ne[1], c[1]);
+  });
+}
+else
+{
+  sw = [-180., -85.];
+  ne = [180., 85.];
+}
+
+if (process.argv.length > 3)
+{
+  NUM_REQUEST = parseInt(process.argv[3]);
+}
 
 // Yes this an own seeded random number generator because its only a few lines
 var seed = 0x1337;
